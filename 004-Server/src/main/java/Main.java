68725045224f77e0bf467d266477efe2a54d4344
maxkvant <max.kvant@gmail.com>
2017-05-04 00:43:06 +0300

    add file load, fixed bugs.

--- 004-Server/src/main/java/Main.java
+++ 004-Server/src/main/java/Main.java
@@ -1,7 +1,9 @@
 import client.Client;
+import com.google.common.io.Files;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,18 +14,24 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import server.ListAnswer;
 import server.Server;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Main extends Application {
     private final Client client = new Client();
     private Server server = null;
-    {
+    private String lastPath = "";
+    static {
         new Thread(() -> {
             try {
                 new Server().start();
@@ -43,29 +51,59 @@ public class Main extends Application {
 
         VBox vBox = new VBox();
         HBox hBox = new HBox();
-        ListView foldersView = new ListView();
-        ListView filesView = new ListView();
+        ListView<String> foldersView = new ListView<>();
+        ListView<String> filesView = new ListView<>();
         TextField pathField = new TextField(".");
         Button showButton = new Button("Show path");
         hBox.getChildren().addAll(pathField, showButton);
 
-        showButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                String path = pathField.getText();
-                try {
-                    List<ListAnswer.Node> nodes = client.executeList(path);
-                    List<String> folders = nodes.stream()
-                            .filter(node -> node.isDirectory)
-                            .map(node -> node.name)
-                            .collect(Collectors.toList());
-                    foldersView.setItems(FXCollections.observableArrayList(folders));
+        Consumer<String> onShowButton = (path) -> {
+            pathField.setText(path);
+            lastPath = path;
+            try {
+                List<ListAnswer.Node> nodes = client.executeList(path);
+                List<String> folders = nodes.stream()
+                        .filter(node -> node.isDirectory)
+                        .map(node -> node.name)
+                        .collect(Collectors.toList());
+                foldersView.setItems(FXCollections.observableArrayList(folders));
 
-                    List<String> files = nodes.stream()
-                            .filter(node -> !node.isDirectory)
-                            .map(node -> node.name)
-                            .collect(Collectors.toList());
-                    filesView.setItems(FXCollections.observableArrayList(files));
+                List<String> files = nodes.stream()
+                        .filter(node -> !node.isDirectory)
+                        .map(node -> node.name)
+                        .collect(Collectors.toList());
+                filesView.setItems(FXCollections.observableArrayList(files));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        pathField.setOnAction(event -> onShowButton.accept(pathField.getText()));
+
+
+        onShowButton.accept(pathField.getText());
+        showButton.setOnMouseClicked(event -> onShowButton.accept(pathField.getText()));
+
+        foldersView.setOnMouseClicked(event -> {
+            String selectedItem = foldersView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                String path = Paths.get(lastPath, selectedItem).toString();
+                onShowButton.accept(path);
+            }
+        });
+
+        filesView.setOnMouseClicked(event -> {
+            String fileName = filesView.getSelectionModel().getSelectedItem();
+            if (fileName != null) {
+                try {
+                    String path = Paths.get(lastPath, fileName).toString();
+                    byte[] bytes = client.executeGet(path);
+                    FileChooser fileChooser = new FileChooser();
+                    fileChooser.setInitialFileName(fileName);
+                    File file = fileChooser.showSaveDialog(primaryStage);
+                    if (file != null) {
+                        Files.write(bytes, file);
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -73,9 +111,11 @@ public class Main extends Application {
         });
 
         hBox.setStyle("-fx-background-color: #336699;");
-        vBox.setSpacing(8);
-        hBox.setSpacing(8);
-        vBox.getChildren().addAll(hBox, foldersView, filesView);
+        vBox.getChildren().addAll(hBox,
+                new Label("Folders:"),
+                foldersView,
+                new Label("Files (click to download):"),
+                filesView);
 
 
         Scene scene = new Scene(vBox, 512, 768);
